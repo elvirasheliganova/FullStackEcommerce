b18552fab82647ec29a17fde9138d5af654f576d
elivra <elvirashelyganova@gmail.com>
2025-04-08 20:13:13 +0200

    install gluestack

--- ecommerce-mobile/App.tsx
+++ ecommerce-mobile/App.tsx
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider mode="light"><View >
+        <Text>Hello World!</Text>
+        <StatusBar style="auto" />
+      </View></GluestackUIProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+;
--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,39 +1,22 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { ProductListItem } from "../components/ProductListItem";
 import products from '../assets/products.json'
-
-
-
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function Page() {
+
   return (
+    <Button variant="outline">
+      <ButtonText>Press me</ButtonText>
+    </Button>
+  )
+  // return (
     
-      <FlatList 
-      data = {products}
-      renderItem={({item}) => <ProductListItem product = {item}/>
-      }
-   />
-  );
+  //     <FlatList 
+  //     data = {products}
+  //     renderItem={({item}) => <ProductListItem product = {item}/>
+  //     }
+  //  />
+  // );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
