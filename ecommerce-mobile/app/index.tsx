e06cc5b5cac054f508bb185bbcfa58ad16da1cea
elivra <elvirashelyganova@gmail.com>
2025-04-08 21:05:32 +0200

    Product Item ui using Gluestack

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,22 +1,21 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import { ProductListItem } from "../components/ProductListItem";
+import  ProductListItem  from "../components/ProductListItem";
 import products from '../assets/products.json'
 import { Button, ButtonText } from "@/components/ui/button";
 
 export default function Page() {
 
-  return (
-    <Button variant="outline">
-      <ButtonText>Press me</ButtonText>
-    </Button>
-  )
-  // return (
+  
+   return (
     
-  //     <FlatList 
-  //     data = {products}
-  //     renderItem={({item}) => <ProductListItem product = {item}/>
-  //     }
-  //  />
-  // );
+      <FlatList 
+       data = {products}
+       numColumns={2}
+       contentContainerClassName="gap-2"
+       columnWrapperClassName="gap-2"
+       renderItem={({item}) => <ProductListItem product = {item}/>
+       }
+    />
+   );
 }
 
